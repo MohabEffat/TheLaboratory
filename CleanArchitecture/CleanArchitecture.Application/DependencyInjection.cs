8234017cbcb77c568f07b07babdf327291fc9784
MohabEffat <99203754+MohabEffat@users.noreply.github.com>
2025-10-08 14:54:35 +0300

    Add Dependeny Injection of each project

---
+++ CleanArchitecture/CleanArchitecture.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchitecture.Application.Services;
+using CleanArchitecture.Core.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<ICarService, CarService>();
+
+            return services;
+        }
+    }
+}
---
+++ CleanArchitecture/CleanArchitecture.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitecture.Core.Interfaces;
+using CleanArchitecture.Infrastructure.Persistence;
+using CleanArchitecture.Infrastructure.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString(""));
+            });
+
+            return services;
+        }
+    }
+}
